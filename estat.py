6764a99245cc83493f0814a4c848ca582c5ed618
Txordi00 <jfrias99@gmail.com>
2016-12-26 03:09:04 +0100

    Comentat una mica més el codi per fer-lo més entenible.

--- estat.py
+++ estat.py
@@ -10,13 +10,13 @@ from matplotlib.legend_handler import HandlerLine2D
 ''' Classe Estat. Aquí emmagatzeno tota la informació rellevant del estat. Gran part de la funcionalitat es subordinarà
  a altres classes/funcions. '''
 class Estat:
-    def __init__(self, m=constants.m_e, k=1, x0=0, p0=0, traslacio=False, kick=False):
+    def __init__(self, coeffs, m=constants.m_e, k=1, x0=0, p0=0, traslacio=False, kick=False):
         self.m = m
         self.x0 = x0
         self.p0 = p0
         self.k = k
-        coeffs = np.array([1,1])
-        self.coeffs = coeffs / np.linalg.norm(coeffs)
+        self.coeffs = coeffs
+        '''Dintre d'estat guardem la funció d'ona.'''
         self.ona = FuncioOna(coeffs=self.coeffs,m=self.m,k=self.k)
 
 ''' Classe Funció d'ona. Aquí avaluem la funció d'ona i, per si es útil consultar-ho, emmagatzenem els darrers x i t, els coeficients
@@ -42,12 +42,14 @@ class FuncioOna:
         return np.polynomial.hermite.hermval(x=(self.x/self.a0), c=hermite_coeffs)
 
     ''' Dibuixem la ona en un rang de X i de T. Aprofitem que només hem de reescriure les línies i no els eixos i tota la resta de la figura
-    per fer-ho de forma molt eficient amb blit. Igualment, '''
+    per fer-ho de forma molt eficient amb blit.'''
     def plot(self, x0=-5e-9, xf=5e-9, t0=0, tf=1, nx=100, nt=1000):
+        '''Valors de les X i T i valors inicials de la funció d'ona Y.'''
         X = np.linspace(x0, xf, nx)
         Y = np.array([self.eval(x=x, t=t0) for x in X])
         T = np.linspace(t0, tf, nt)
 
+        '''Definició/Inicialització de tots els plots.'''
         fig, ax = plt.subplots()
         linia_real = ax.plot(X, Y.real, 'b', label='Real', animated=True)[0]
         linia_imag = ax.plot(X, Y.imag, 'r', label='Imaginari', animated=True)[0]
@@ -57,35 +59,24 @@ class FuncioOna:
         ax.legend(handles, labels)
         lines = [linia_real, linia_imag]
 
+        '''Funció auxiliar per computar la animació. Avalua per cada temps la funció d'ona Y als X donats.'''
         def animate(t):
             Y = np.array([self.eval(x=x, t=t) for x in X])
             lines[0].set_ydata(Y.real)
             lines[1].set_ydata(Y.imag)
             return lines
 
+        '''Animació eficient. '''
         ani = animation.FuncAnimation(fig, animate, T,
                                       interval=100, blit=True, repeat=False)
 
         plt.show()
 
-            # for t in T:
-        #     Y = np.array([self.eval(x=x, t=t) for x in X])
-        #     linia_real, = plt.plot(X, Y.real, 'b', label='Real')
-        #     linia_imag, = plt.plot(X, Y.imag, 'r', label='Imaginari')
-        #     plt.legend(handler_map={linia_real: HandlerLine2D()})
-        #     plt.xlim(np.min(X), np.max(X))
-        #     plt.ylim(-100000, 100000)
-        #     plt.show()
-        #     plt.pause(0.01)
-        #     plt.clf()
-
-
+'''Main per fer petites proves'''
 if __name__ == '__main__':
     coeffs = np.array([1,1])
     coeffs = coeffs / np.linalg.norm(coeffs)
-    # ona = FuncioOna(coeffs=coeffs, m=constants.m_e)
-    estat = Estat(m=constants.m_e,k=1)
+    estat = Estat(coeffs=coeffs, m=constants.m_e, k=1)
     estat.ona.plot(x0=-5e-9,xf=5e-9,t0=0,tf=1,nx=100,nt=1000)
 
-
-    print('Sortida')
+    print('Sortida Correcta')
