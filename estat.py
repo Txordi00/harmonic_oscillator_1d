e6e9ac9aa6bfc64d3a64e6e9fac30ea04dd47390
Txordi00 <jfrias99@gmail.com>
2016-12-27 17:04:45 +0100

    Està programat el procediment per calcular els coeficients però no funciona bé i no sé perquè...

--- estat.py
+++ estat.py
@@ -4,9 +4,29 @@ import cmath
 import numpy as np
 from scipy import constants
 from scipy import misc
+from scipy import integrate
 import matplotlib.pylab as plt
 import matplotlib.animation as animation
 from matplotlib.legend_handler import HandlerLine2D
+
+INF = 1e6
+
+'''Energía per cada n'''
+def E_n(n, omega):
+    return constants.hbar * omega * (float(n) + 1/2)
+
+'''Part temporal de la solució de l'equació d'Schrodinger depenent del temps.'''
+def exp_t(E_n,t):
+    return cmath.exp(-1j*E_n*t/constants.hbar)
+
+'''Solucions estàcionaries de l'equació d'Schrodinger.'''
+def phi_n(x, n, a0):
+    coeff = np.zeros((n+1,))
+    coeff[n] = 1
+    fn = 1/( math.pi**(1/4) * math.sqrt(2**n*math.factorial(n)*a0) )
+    xbar = x/a0
+    fx = math.exp(-xbar**2/2) * np.polynomial.hermite.hermval(x=xbar, c=coeff)
+    return fn * fx
 
 ''' Classe Estat. Aquí emmagatzemo tota la informació rellevant del estat. Gran part de la funcionalitat es subordinarà
  a altres classes/funcions. '''
@@ -18,13 +38,19 @@ class Estat:
         '''Dintre d'estat guardem la funció d'ona.'''
         self.ona = FuncioOna(coeffs=self.coeffs,m=self.m,k=self.k)
 
-    '''Fer Kick implica donar-li un impuls p0 al moment emmagatzemat a la funció d'ona.'''
+    '''Fer Kick implica donar-li un impuls p0 al moment emmagatzemat a la funció d'ona. Posteriorment recalculem la descomposició
+    de la ona en les funcions pròpies.'''
     def kick(self,p0):
         self.ona.p0 = self.ona.p0 + p0
+        self.ona.update_coeffs()
+        self.coeffs = self.ona.coeffs
 
-    '''Trasladar implica aplicar un desplaçament a la x0 emmagatzemada a la funció d'ona.'''
+    '''Trasladar implica aplicar un desplaçament a la x0 emmagatzemada a la funció d'ona. Posteriorment recalculem la descomposició
+    de la ona en les funcions pròpies.'''
     def traslacio(self,x0):
         self.ona.x0 = self.ona.x0 + x0
+        self.ona.update_coeffs()
+        self.coeffs = self.ona.coeffs
 
 ''' Classe Funció d'ona. Aquí avaluem la funció d'ona i, per si es útil consultar-ho, emmagatzenem els darrers x i t, els coeficients
  i les constants a0 i omega. '''
@@ -39,17 +65,9 @@ class FuncioOna:
 
     ''' Funcionalitat Important. Avaluem la ona als diferents punts (sense fer ni kick ni translació).'''
     def eval0(self,x,t):
-        self.x = x
-        self.t = t
-        N = np.size(coeffs)
+        N = np.size(self.coeffs)
 
-        ''' Calculem els coeficients que multiplicaràn als polinomis d'Hermite i després els multipliquem per ells i ho sumem
-        amb la funció np.polynomial.hermite.hermval. '''
-        hermite_coeffs = [ coeffs[n]*1/( math.pi**(1/4) * math.sqrt(2**n*math.factorial(n)*self.a0) ) * math.exp(-(self.x/self.a0)**2 / 2) # f(n)*g(x)
-                           * cmath.exp(-1j*self.omega*(float(n)+1/2)*self.t) for n in range(N)] # h(t)
-        hermite_coeffs = np.array(hermite_coeffs)
-
-        return  np.polynomial.hermite.hermval(x=(self.x/self.a0), c=hermite_coeffs)
+        return np.sum([self.coeffs[n]*phi_n(x=x,n=n,a0=self.a0) * exp_t(E_n=E_n(n,self.omega),t=t) for n in range(N)])
 
     '''Apliquem el Kick.'''
     def eval1(self,x,t):
@@ -61,10 +79,31 @@ class FuncioOna:
         # '''Defineixo aquesta funció auxiliar perquè només volem calcular la parcial respecte de x.'''
         # def fx(x):
         #     return self.eval1(x,t)
-        # '''Aproximació Taylor de grau 1 de U(x0)*f(x,t) = exp(-i*x0*p/hbar)*f(x,t)'''
+        # '''Aproximació Taylor de grau 1 de U(x0)*f(x,t) = exp(-i*x0*p/hbar)*f(x,t). Això sol serveix per x molt petites,
+        #   en un cas real hauriem de aproximar amb més graus.'''
         # return fx(x) - self.x0*misc.derivative(fx,x,dx=1e-18)
         '''Però, com que sabem que U(x0)*f(x,t) = f(x-x0,t):'''
-        return self.eval1(x-self.x0,t)
+        return self.eval1(float(x)-self.x0,t)
+
+    '''Actualitzem els coeficients calculant la integral de phi_n'(x)*ona(x,0).'''
+    def update_coeffs(self):
+        '''Definim els integrands'''
+        integrand_real = lambda x, n: (np.conj(phi_n(x=x,n=n,a0=self.a0)) * self.eval(x=x,t=0)).real
+        integrand_imag = lambda x, n: (np.conj(phi_n(x=x,n=n,a0=self.a0)) * self.eval(x=x,t=0)).imag
+
+        '''Calculem els coeficients cn fins que la suma de les probabilitats excedeix 0.99'''
+        accum_prob = 0
+        n=0
+        coeffs_aux = []
+        while (accum_prob<0.99):
+            # cn = integrate.quadrature(func=integrand_real,a=-INF,b=+INF,args=(n,),vec_func=False,tol=1e-18,rtol=1e-18)[0] + 1j*integrate.quadrature(func=integrand_imag,a=-INF,b=+INF,args=(n,),vec_func=False,tol=1e-18,rtol=1e-18)[0]
+            cn = integrate.quad(func=integrand_real, a=-np.inf, b=+np.inf, args=(n,))[0] + 1j * integrate.quad(func=integrand_imag, a=-np.inf, b=+np.inf, args=(n,))[0]
+            coeffs_aux = coeffs_aux + [cn]
+            accum_prob = accum_prob + abs(cn)**2
+            n = n + 1
+            print(str(n) + ' coeficients amb precissió ' + str(accum_prob))
+        self.coeffs = np.array(coeffs_aux)
+
 
     ''' Dibuixem la ona en un rang de X i de T. Aprofitem que només hem de reescriure les línies i no els eixos i tota la resta de la figura
     per fer-ho de forma molt eficient amb blit.'''
@@ -99,7 +138,7 @@ class FuncioOna:
 
 '''Main per fer petites proves'''
 if __name__ == '__main__':
-    coeffs = np.array([1,1])
+    coeffs = np.array([1,0])
     coeffs = coeffs / np.linalg.norm(coeffs)
     estat = Estat(coeffs=coeffs, m=constants.m_e, k=1)
 
