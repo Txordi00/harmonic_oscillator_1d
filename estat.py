5987272604d8c0af5ecf22246c4a4371a39a805b
Txordi00 <jfrias99@gmail.com>
2017-01-07 17:00:01 +0100

    Millor plot pero no puc fer bé el de la std :(

--- estat.py
+++ estat.py
@@ -5,12 +5,47 @@ import numpy as np
 ##TODO: Arreglar import scipy as sp
 # from scipy import constants
 from scipy import integrate
+from scipy.misc import derivative
 import matplotlib.pylab as plt
 import matplotlib.animation as animation
 
 HBAR = 1
 M = 1
 K = 1
+
+def adjust_err_bar(errobj, x, y, x_error, y_error):
+    ln, (errx_top, errx_bot, erry_top, erry_bot), (barsx, barsy) = errobj
+
+    if(not isinstance(x, np.ndarray)):
+        x_base = np.array([x])
+    else:
+        x_base = x
+    if(not isinstance(y, np.ndarray)):
+        y_base = np.array([y])
+    else:
+        y_base = y
+
+    ln.set_data(x_base, y_base)
+    xerr_top = x_base + x_error
+    xerr_bot = x_base - x_error
+    yerr_top = y_base + y_error
+    yerr_bot = y_base - y_error
+
+    errx_top.set_xdata(xerr_top)
+    errx_bot.set_xdata(xerr_bot)
+    errx_top.set_ydata(y_base)
+    errx_bot.set_ydata(y_base)
+
+    erry_top.set_xdata(x_base)
+    erry_bot.set_xdata(x_base)
+    erry_top.set_ydata(yerr_top)
+    erry_bot.set_ydata(yerr_bot)
+
+    new_segments_x = [np.array([[xt, y], [xb,y]]) for xt, xb, y in zip(xerr_top, xerr_bot, y_base)]
+    new_segments_y = [np.array([[x, yt], [x,yb]]) for x, yt, yb in zip(x_base, yerr_top, yerr_bot)]
+    # print(new_segments_x)
+    barsx.set_segments(new_segments_x)
+    barsy.set_segments(new_segments_y)
 
 '''Energía per cada n'''
 def E_n(n, omega):
@@ -109,15 +144,35 @@ class FuncioOna:
         self.coeffs = np.array(coeffs_aux)
 
 
+    def expected_value(self, operator, t):
+        func_prob = lambda x, t: abs(self.eval(x,t))**2
+        func_x = lambda x,t: func_prob(x,t)*x
+        func_x2 = lambda x,t: func_prob(x,t)*x**2
+        func_p = lambda x,t: np.conj(self.eval(x,t)) * HBAR/1j * derivative(func=self.eval,x0=x,dx=1e-2,n=1,args=(t,))
+        func_p2 = lambda x,t: np.conj(self.eval(x,t)) * (-HBAR**2) * derivative(func=self.eval,x0=x,dx=1e-2,n=2,args=(t,))
+
+        if(operator.lower()=='x'):
+            return integrate.quad(func=func_x, a=-np.inf, b=+np.inf, args=(t,))[0]
+        elif(operator.lower()=='x2'):
+            return integrate.quad(func=func_x2, a=-np.inf, b=+np.inf, args=(t,))[0]
+        elif (operator.lower() == 'p'):
+            return integrate.quad(func=func_p, a=-np.inf, b=+np.inf, args=(t,))[0]
+        elif (operator.lower() == 'p2'):
+            return integrate.quad(func=func_p2, a=-np.inf, b=+np.inf, args=(t,))[0]
+
+
+
     ''' Dibuixem la ona en un rang de X i de T. Aprofitem que només hem de reescriure les línies i no els eixos i tota la resta de la figura
-    per fer-ho de forma molt eficient amb blit.'''
-    def plot(self, x0=-5e-9, xf=5e-9, t0=0, tf=1, nx=100, nt=1000):
+    per fer-ho de forma eficient amb blit.'''
+    def plot(self, x0=-10, xf=10, t0=0, tf=10, nx=480, nt=100):
         '''Valors de les X i T i valors inicials de la funció d'ona Y.'''
         X = np.linspace(x0, xf, nx)
         T = np.linspace(t0, tf, nt)
         V = 1./2.*self.k*X**2
         print('Calculant els valors de la funció de ona...')
         Y = [np.array([self.eval(x=x, t=t) for x in X]) for t in T]
+        EXP_VAL = np.array([self.expected_value(operator='x',t=t) for t in T])
+        STD = np.sqrt(np.array([self.expected_value(operator='x2',t=t) for t in T]) - EXP_VAL**2)
 
         '''Definició/Inicialització de tots els plots.'''
         fig, ax = plt.subplots()
@@ -127,22 +182,36 @@ class FuncioOna:
 
         linia_real = ax.plot(X, Y[0].real, 'b', label='Re[Ona]', animated=True)[0]
         linia_imag = ax.plot(X, Y[0].imag, 'r', label='Im[Ona]', animated=True)[0]
-        linia_pot = ax.plot(X, V-ylim, 'g', label='Potencial', animated=True)[0]
+        linia_prob = ax.plot(X, abs(Y[0])**2, 'y', label='Prob', animated=True)[0]
+        linia_exp = ax.plot([EXP_VAL[0],EXP_VAL[0]], [-ylim,ylim], 'k', label='Exp_val', animated=True)[0]
+        # linia_std, bottoms_tops, _ = ax.errorbar(EXP_VAL[0], ylim/4., xerr=STD[0], yerr=0, label='Std', animated=True)
+        errobj = ax.errorbar(EXP_VAL[0], ylim / 4., xerr=STD[0], yerr=0, label='Std', animated=True)
+        linia_pot = ax.plot(X, V-ylim, 'g', label='Potencial', animated=False)[0]
         handles, labels = ax.get_legend_handles_labels()
         ax.legend(handles, labels)
-        lines = [linia_real, linia_imag, linia_pot]
+        lines = [linia_real, linia_imag, linia_prob, linia_exp, linia_pot]
 
         '''Funció auxiliar per computar la animació. Avalua per cada temps la funció d'ona Y als X donats.'''
         def animate(n):
             # Y = np.array([self.eval(x=x, t=t) for x in X])
             lines[0].set_ydata(Y[n].real)
             lines[1].set_ydata(Y[n].imag)
+            lines[2].set_ydata(abs(Y[n])**2)
+            lines[3].set_xdata([EXP_VAL[n],EXP_VAL[n]])
+            adjust_err_bar(errobj=errobj,x=EXP_VAL[n],y=ylim/4.,x_error=STD[n],y_error=0)
+            # bottoms_tops[0].set_xdata(EXP_VAL[n]-STD[n])
+            # bottoms_tops[1].set_xdata(EXP_VAL[n] + STD[n])
+            # lines[4].set_xdata(EXP_VAL[n])
             return lines
 
-        '''Animació eficient. '''
-        ani = animation.FuncAnimation(fig, animate, range(len(Y)),
+        '''Animació eficient.'''
+        ani = animation.FuncAnimation(fig, animate, range(len(T)),
                                       interval=100, blit=True, repeat=False)
         plt.show()
+
+def prova(a):
+    a = a + 1
+    print(a)
 
 '''Main per fer petites proves'''
 if __name__ == '__main__':
@@ -152,6 +221,10 @@ if __name__ == '__main__':
     tf = 10
     nx = 480
     nt = 100
+    a = 0
+    print(a)
+    prova(a)
+    print(a)
 
     coeffs = np.array([1])
     estat = Estat(coeffs=coeffs, m=M, k=1)
@@ -164,14 +237,15 @@ if __name__ == '__main__':
     # print('Àrea ona: ' + str(abs(I_ona)**2))
 
     estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
+    print('Valor esperat: ' + str(estat.ona.expected_value(operator='x',t=0)))
 
 
-    estat.kick(p0=8)
-    estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
-
-    estat.kick(p0=-8)
-    estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
-
+    # estat.kick(p0=8)
+    # estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
+    #
+    # estat.kick(p0=-8)
+    # estat.ona.plot(x0=x0,xf=xf,t0=tf+t0,tf=tf+tf,nx=nx,nt=nt)
+    #
     estat.traslacio(x0=1)
     estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
 
