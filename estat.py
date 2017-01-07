fc998631671851b009397d5ee0195479e49f24ba
Txordi00 <jfrias99@gmail.com>
2017-01-07 22:59:04 +0100

    Ja funciona bé tota l'animació!!

--- estat.py
+++ estat.py
@@ -2,50 +2,17 @@
 import math
 import cmath
 import numpy as np
-##TODO: Arreglar import scipy as sp
 # from scipy import constants
 from scipy import integrate
 from scipy.misc import derivative
 import matplotlib.pylab as plt
 import matplotlib.animation as animation
+import sys
+from funcions_extra import *
 
 HBAR = 1
 M = 1
 K = 1
-
-def adjust_err_bar(errobj, x, y, x_error, y_error):
-    ln, (errx_top, errx_bot, erry_top, erry_bot), (barsx, barsy) = errobj
-
-    if(not isinstance(x, np.ndarray)):
-        x_base = np.array([x])
-    else:
-        x_base = x
-    if(not isinstance(y, np.ndarray)):
-        y_base = np.array([y])
-    else:
-        y_base = y
-
-    ln.set_data(x_base, y_base)
-    xerr_top = x_base + x_error
-    xerr_bot = x_base - x_error
-    yerr_top = y_base + y_error
-    yerr_bot = y_base - y_error
-
-    errx_top.set_xdata(xerr_top)
-    errx_bot.set_xdata(xerr_bot)
-    errx_top.set_ydata(y_base)
-    errx_bot.set_ydata(y_base)
-
-    erry_top.set_xdata(x_base)
-    erry_bot.set_xdata(x_base)
-    erry_top.set_ydata(yerr_top)
-    erry_bot.set_ydata(yerr_bot)
-
-    new_segments_x = [np.array([[xt, y], [xb,y]]) for xt, xb, y in zip(xerr_top, xerr_bot, y_base)]
-    new_segments_y = [np.array([[x, yt], [x,yb]]) for x, yt, yb in zip(x_base, yerr_top, yerr_bot)]
-    # print(new_segments_x)
-    barsx.set_segments(new_segments_x)
-    barsy.set_segments(new_segments_y)
 
 '''Energía per cada n'''
 def E_n(n, omega):
@@ -143,7 +110,7 @@ class FuncioOna:
         print(str(n) + ' coeficients amb precissió ' + str(accum_prob))
         self.coeffs = np.array(coeffs_aux)
 
-
+    '''Funcio per calcular els valors esperats. Per seleccionar el valor què vols s'ha de escollir un operador x, x2, p, p2.'''
     def expected_value(self, operator, t):
         func_prob = lambda x, t: abs(self.eval(x,t))**2
         func_x = lambda x,t: func_prob(x,t)*x
@@ -159,6 +126,8 @@ class FuncioOna:
             return integrate.quad(func=func_p, a=-np.inf, b=+np.inf, args=(t,))[0]
         elif (operator.lower() == 'p2'):
             return integrate.quad(func=func_p2, a=-np.inf, b=+np.inf, args=(t,))[0]
+        else:
+            sys.exit('El valor de operador ha de ser un string d\'aquests: \'x\', \'x2\', \'p\', \'p2\'')
 
 
 
@@ -185,7 +154,8 @@ class FuncioOna:
         linia_prob = ax.plot(X, abs(Y[0])**2, 'y', label='Prob', animated=True)[0]
         linia_exp = ax.plot([EXP_VAL[0],EXP_VAL[0]], [-ylim,ylim], 'k', label='Exp_val', animated=True)[0]
         # linia_std, bottoms_tops, _ = ax.errorbar(EXP_VAL[0], ylim/4., xerr=STD[0], yerr=0, label='Std', animated=True)
-        errobj = ax.errorbar(EXP_VAL[0], ylim / 4., xerr=STD[0], yerr=0, label='Std', animated=True)
+        errobj = ax.errorbar(EXP_VAL[0], ylim/4., color='k', xerr=STD[0], yerr=0, label='Std', animated=True)
+        # linia_err = errobj[0]
         linia_pot = ax.plot(X, V-ylim, 'g', label='Potencial', animated=False)[0]
         handles, labels = ax.get_legend_handles_labels()
         ax.legend(handles, labels)
@@ -198,20 +168,13 @@ class FuncioOna:
             lines[1].set_ydata(Y[n].imag)
             lines[2].set_ydata(abs(Y[n])**2)
             lines[3].set_xdata([EXP_VAL[n],EXP_VAL[n]])
-            adjust_err_bar(errobj=errobj,x=EXP_VAL[n],y=ylim/4.,x_error=STD[n],y_error=0)
-            # bottoms_tops[0].set_xdata(EXP_VAL[n]-STD[n])
-            # bottoms_tops[1].set_xdata(EXP_VAL[n] + STD[n])
-            # lines[4].set_xdata(EXP_VAL[n])
-            return lines
+            lines_err = adjust_err_bar(errobj=errobj,x=EXP_VAL[n],y=ylim/4.,x_error=STD[n],y_error=0)
+            return lines + lines_err
 
         '''Animació eficient.'''
         ani = animation.FuncAnimation(fig, animate, range(len(T)),
                                       interval=100, blit=True, repeat=False)
         plt.show()
-
-def prova(a):
-    a = a + 1
-    print(a)
 
 '''Main per fer petites proves'''
 if __name__ == '__main__':
@@ -221,10 +184,6 @@ if __name__ == '__main__':
     tf = 10
     nx = 480
     nt = 100
-    a = 0
-    print(a)
-    prova(a)
-    print(a)
 
     coeffs = np.array([1])
     estat = Estat(coeffs=coeffs, m=M, k=1)
@@ -237,16 +196,16 @@ if __name__ == '__main__':
     # print('Àrea ona: ' + str(abs(I_ona)**2))
 
     estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
-    print('Valor esperat: ' + str(estat.ona.expected_value(operator='x',t=0)))
+
+    estat.traslacio(x0=1)
+    estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
 
 
-    # estat.kick(p0=8)
-    # estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
+    estat.kick(p0=2)
+    estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
     #
     # estat.kick(p0=-8)
     # estat.ona.plot(x0=x0,xf=xf,t0=tf+t0,tf=tf+tf,nx=nx,nt=nt)
     #
-    estat.traslacio(x0=1)
-    estat.ona.plot(x0=x0,xf=xf,t0=t0,tf=tf,nx=nx,nt=nt)
 
     print('Sortida Correcta')
